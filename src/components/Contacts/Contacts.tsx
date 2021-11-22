50272be449941c831bcd46335f34ab4d795db18e
Pavel Kardash <cardash@mail.ru>
2021-11-22 22:06:20 +0300

    done mob ver potfolio

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,18 +1,32 @@
-
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "./Contacts.module.scss";
-import container from "../Common/Styles/Container.module.scss";
-export const Contacts =() => {
- return (
-     <div className={styles.wrapper}>
-         <div className={styles.fullscreen}>
-             <div className={styles.fullscreen__body}>
-                 <div className={styles.content}>
-                     <div className={styles.content__title}>Contacts</div>
-                 </div>
-             </div>
-         </div>
-     </div>
+
+export const Contacts = () => {
+
+    const [customStyle, setCustomStyle] = useState(styles.wrapper)
+
+
+    useEffect(() => {
+
+
+        setTimeout(() => setCustomStyle(styles.test), 0)
+
+        return () => {
+            setCustomStyle(styles.wrapper)
+        }
+    }, [])
+
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.fullscreen}>
+                <div className={styles.fullscreen__body}>
+                    <div className={styles.content}>
+                        <div className={styles.content__title}>Contacts</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,19 +1,123 @@
-
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "./Portfolio.module.scss";
-import container from "../Common/Styles/Container.module.scss";
 
-export const Portfolio =() => {
- return (
-     <div className={styles.wrapper}>
-         <div className={styles.fullscreen}>
-             <div className={styles.fullscreen__body}>
-                 <div className={styles.content}>
-                     <div className={styles.content__title}>Portfolio</div>
-                 </div>
-             </div>
-         </div>
-     </div>
+export const Portfolio = () => {
+
+    const [customStyle, setCustomStyle] = useState(styles.wrapper)
+
+
+    useEffect(() => {
+
+
+        setTimeout(() => setCustomStyle(styles.test), 0)
+
+        return () => {
+            setCustomStyle(styles.wrapper)
+        }
+    }, [])
+
+
+    return (
+        <div className={customStyle}>
+
+            <div className={styles.fullscreen}>
+                <div className={styles.fullscreen__body}>
+                    <div className={styles.content}>
+                        <div className={styles.content__title}>MY <span>PORTFOLIO</span></div>
+                        <div className={styles.content__subtitle}>A FEW RECENT DESIGN AND CODING PROJECTS. WANT TO
+                            SEE
+                            MORE? EMAIL ME.
+                        </div>
+                        <div className={styles.portfolio__body}>
+                            <div className={styles.portfolio__row}>
+                                <div className={styles.portfolio__column}>
+                                    <div className={styles.portfolio__item}>
+                                        <div className={styles.portfolio__image}><img src="img/" alt=""/></div>
+                                        <div className={styles.portfolio__footer}>
+                                            <div className={styles.portfolio__code}>Code</div>
+                                            <div className={styles.portfolio__demo}>Demo</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.portfolio__column}>
+                                    <div className={styles.portfolio__item}>
+                                        <div className={styles.portfolio__image}><img src="img/" alt=""/></div>
+                                        <div className={styles.portfolio__footer}>
+                                            <div className={styles.portfolio__code}>Code</div>
+                                            <div className={styles.portfolio__demo}>Demo</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.portfolio__column}>
+                                    <div className={styles.portfolio__item}>
+                                        <div className={styles.portfolio__image}><img src="img/" alt=""/></div>
+                                        <div className={styles.portfolio__footer}>
+                                            <div className={styles.portfolio__code}>Code</div>
+                                            <div className={styles.portfolio__demo}>Demo</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.portfolio__column}>
+                                    <div className={styles.portfolio__item}>
+                                        <div className={styles.portfolio__image}><img src="img/" alt=""/></div>
+                                        <div className={styles.portfolio__footer}>
+                                            <div className={styles.portfolio__code}>Code</div>
+                                            <div className={styles.portfolio__demo}>Demo</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.portfolio__column}>
+                                    <div className={styles.portfolio__item}>
+                                        <div className={styles.portfolio__image}><img src="img/" alt=""/></div>
+                                        <div className={styles.portfolio__footer}>
+                                            <div className={styles.portfolio__code}>Code</div>
+                                            <div className={styles.portfolio__demo}>Demo</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.portfolio__column}>
+                                    <div className={styles.portfolio__item}>
+                                        <div className={styles.portfolio__image}><img src="img/" alt=""/></div>
+                                        <div className={styles.portfolio__footer}>
+                                            <div className={styles.portfolio__code}>Code</div>
+                                            <div className={styles.portfolio__demo}>Demo</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.portfolio__column}>
+                                    <div className={styles.portfolio__item}>
+                                        <div className={styles.portfolio__image}><img src="img/" alt=""/></div>
+                                        <div className={styles.portfolio__footer}>
+                                            <div className={styles.portfolio__code}>Code</div>
+                                            <div className={styles.portfolio__demo}>Demo</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.portfolio__column}>
+                                    <div className={styles.portfolio__item}>
+                                        <div className={styles.portfolio__image}><img src="img/" alt=""/></div>
+                                        <div className={styles.portfolio__footer}>
+                                            <div className={styles.portfolio__code}>Code</div>
+                                            <div className={styles.portfolio__demo}>Demo</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.portfolio__column}>
+                                    <div className={styles.portfolio__item}>
+                                        <div className={styles.portfolio__image}><img src="img/" alt=""/></div>
+                                        <div className={styles.portfolio__footer}>
+                                            <div className={styles.portfolio__code}>Code</div>
+                                            <div className={styles.portfolio__demo}>Demo</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     );
 };
 
